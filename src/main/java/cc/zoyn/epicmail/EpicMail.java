e45c867732918b95bdfdc4fabfcef275f902af95
Zoyn <602723113@qq.com>
2018-05-30 20:57:24 +0800

    :art: add sqlite connect method

--- src/main/java/cc/zoyn/epicmail/EpicMail.java
+++ src/main/java/cc/zoyn/epicmail/EpicMail.java
@@ -9,9 +9,12 @@ import cc.zoyn.epicmail.util.StreamUtils;
 import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.javalite.activejdbc.Base;
+import org.javalite.activejdbc.DB;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import static org.bukkit.Bukkit.getPluginCommand;
 import static org.bukkit.Bukkit.getPluginManager;
@@ -34,6 +37,15 @@ public class EpicMail extends JavaPlugin {
     private StorageType storageType;
     @Getter
     private File sqliteFile;
+
+    @Getter
+    private DB db;
+
+    private String databaseHost;
+    private String databasePort;
+    private String databaseUserName;
+    private String databasePassword;
+    private String database;
 
     @Override
     public void onLoad() {
@@ -58,27 +70,56 @@ public class EpicMail extends JavaPlugin {
 
         // default language is zh-CN
         language = getConfig().getString("language", "zh-CN");
+        Logger.info("set language: " + language);
 
         // check storage type
         storageType = StorageType.valueOf(getConfig().getString("storageType"));
+
+        databaseHost = getConfig().getString("databaseOption.ip");
+        databasePort = getConfig().getString("databaseOption.port");
+        databaseUserName = getConfig().getString("databaseOption.user");
+        databasePassword = getConfig().getString("databaseOption.password");
+        database = getConfig().getString("databaseOption.database");
+
+        boolean tableIsCreated = false;
         if (storageType.equals(StorageType.SQLITE)) {
             sqliteFile = new File(getDataFolder(), "mail.db");
             if (!sqliteFile.exists()) {
                 try {
-                    sqliteFile.createNewFile();
+                    tableIsCreated = sqliteFile.createNewFile();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+            db = Base.open("org.sqlite.JDBC", "jdbc:sqlite:" + sqliteFile.getAbsolutePath(), databaseUserName, databasePassword);
+            // check the table is created
+            if (tableIsCreated) {
+                try {
+                    db.startBatch("CREATE TABLE IF NOT EXISTS epicmail_mail( " +
+                            "id INT PRIMARY KEY       NOT NULL, " +
+                            "title          TEXT      NOT NULL, " +
+                            "sender         TEXT      NOT NULL, " +
+                            "reciver        CHAR(50)  NOT NULL, " +
+                            "message        TEXT      NOT NULL, " +
+                            "items          TEXT      NOT NULL, " +
+                            "send_time      REAL      NOT NULL);")
+                            .execute();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else if (storageType.equals(StorageType.MYSQL)) {
+            // jdbc:mysql//localhost:3306/mc
+            db = Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + database, databaseUserName, databasePassword);
         }
+
+        Logger.info("setting storage type: " + storageType.toString());
 
         // command registe
         getPluginCommand("epicmail").setExecutor(new CommandHandler());
-
         // listener registe
         getPluginManager().registerEvents(new PlayerListener(), this);
-
-        Logger.info("set language: " + language);
     }
 
     /**
